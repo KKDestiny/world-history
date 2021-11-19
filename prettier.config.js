c3e76d0816786202a7aa8a7f7c9f71e33cdc401b
linxiaozhou <1139904786@qq.com>
2021-11-19 22:51:25 +0800

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: linxiaozhou.com
+ * @LastEditors: linxiaozhou.com
+ * @Description: file content
+ */
+module.exports = {
+  singleQuote: false,
+  traillingComma: 'all',
+  arrowParens: 'avoid'
+}
